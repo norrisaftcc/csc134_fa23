4d4a598eaa36a009001f35eab07ec91896bdbc9d
teacherbot <norrisa@faytechcc.edu>
2023-08-28 11:25:52 -0400

    Started m1LAB

---
+++ M1LAB_Norris/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+CSC 134
+M1LAB - Apple Orchard
+norrisa
+8/28
+Simple interactive store.
+*/
+
+
+int main()
+{
+    string name = "Norris";
+    cout << "Welcome to the " << name << " orchard." << endl;
+    return 0;
+}
