b34183906e06105c5455395d4e668443129840f0
teacherbot <norrisa@faytechcc.edu>
2023-08-28 11:35:56 -0400

    First non-interactive version done

--- M1LAB_Norris/main.cpp
+++ M1LAB_Norris/main.cpp
@@ -15,5 +15,21 @@ int main()
     string name = "Norris";
     cout << "Welcome to the " << name << " orchard." << endl;
 
+    // declare our variables
+    int apples = 100;
+    double price_each = 0.25;
+
+    // print number of apples, price each
+    cout << "We have " << apples << " apples for sale." << endl;
+    cout << "Cost is $" << price_each << " each." << endl;
+
+    double total_price = apples * price_each;
+    // then print the total price
+    cout << "The price for all of them is $" << total_price << endl;
+    // TODO: ask the user how many to buy
+
+    // TODO: Make prices look "Correct" (e.g. $25.00)
+
+
     return 0;
 }
