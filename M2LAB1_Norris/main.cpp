ea91a621d5b087d64307587aa4fd4026a4aba6b4
teacherbot <norrisa@faytechcc.edu>
2023-09-13 10:52:26 -0400

    Started project

---
+++ M2LAB1_Norris/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+/*
+M2LAB1 - Crate Example
+Find the volume of a crate of a given size
+Find the cost to make it
+Find the price we sell it for
+Find the profit
+
+*/
+
+using namespace std;
+
+int main()
+{
+    cout << "General Crates Pricing System" << endl;
+    // declare the variables
+    const double COST_PER_CUBIC_FOOT = 0.23; // $ per cubic foot
+    const double CHARGE_PER_CUBIC_FOOT = 0.5; // $ per cubic foot
+
+    double length, width, height; // in feet
+    double volume; // in cubic feet
+    double cost, charge, profit; // $
+
+    //
+
+
+
+    return 0;
+}
